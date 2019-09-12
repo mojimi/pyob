ebed799e8be9f7c1524b9ece1bb4561cc91314ce
Gabriel Canto <gabriel.canto@corp.carusojrea.com.br>
2019-09-12 18:55:56 -0300

    initial poc

---
+++ src/character.py
@@ -0,0 +1,25 @@
+class Character:
+    __init__(self):
+
+
+class SkillGem:
+    pass
+
+class Dexterity:
+    pass
+
+class Melee:
+    pass
+
+class Attack:
+    pass
+
+class Cyclone(DexterityGem):
+
+    tags = ['melee', 'channeling', 'physical']
+    multiplier
+
+    def get_dps(self):
+        self.character
+
+
